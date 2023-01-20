410183ba9ff93e5dd476429d50bbd93220d639db
FelixKrayer <felix@krayernet.eu>
2023-01-20 09:08:37 +0100

    transfer

--- code/analyses.ml
+++ code/analyses.ml
@@ -1,24 +1,27 @@
 module type Spec =
 sig
+  (* Domain *)
   module D : Lattice.S
   module C : Printable.S
 
   val name : unit -> string
-
   val startstate : varinfo -> D.t
 
+  (* Transfer functions *)
   val assign: (D.t, C.t) ctx -> lval -> exp -> D.t
   val vdecl : (D.t, C.t) ctx -> varinfo -> D.t
   val branch: (D.t, C.t) ctx -> exp -> bool -> D.t
-  val return: (D.t, C.t) ctx -> exp option  -> fundec -> D.t
   val query : (D.t, C.t) ctx -> 'a Queries.t -> 'a Queries.result
 
+  (* Functions for interprocedural analysis *)
   val special : (D.t, C.t) ctx -> lval option -> varinfo -> exp list -> D.t
   val enter   : (D.t, C.t) ctx -> lval option -> fundec -> exp list -> (D.t * D.t) list
+  val return  : (D.t, C.t) ctx -> exp option  -> fundec -> D.t
   val combine : (D.t, C.t) ctx -> lval option -> exp -> fundec -> exp list -> C.t option -> D.t -> D.t
 
   val context : fundec -> D.t -> C.t
 
+  (* Function for analysis of multithreaded programs *)
   val threadenter : (D.t, C.t) ctx -> lval option -> varinfo -> exp list -> D.t list
   val threadspawn : (D.t, C.t) ctx -> lval option -> varinfo -> exp list -> (D.t, C.t) ctx -> D.t
 end
