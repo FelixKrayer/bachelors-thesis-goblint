41bea58c7718a2badb936d114a36b2ef03db8707
FelixKrayer <felix@krayernet.eu>
2023-02-08 12:53:18 +0100

    intro done (mostly)

--- code/01-example_intro.c
+++ code/01-example_intro.c
@@ -1,22 +1,12 @@
-int function (int a) {
-  //a = [0, 12]; y = [1, 2]
-  a = a * 2;
-  return a; //x_f = [0, 24]; y = [1, 2]
+void function() {
+  //...
 }
 
-int y; //global
+int glob;
+void main() {
+  glob = 1;
+  function();|\label{code:call1}|
 
-int main() {
-  int x; //local
-  x = 0;
-
-  y = 1;
-  x = function(0); // a = x = [0, 0]; y = [1, 1]
-  // x = [0, 24], y = [1, 1]
-
-  //...
-
-  y = 2;
-  x = function(12); // a = x = [12, 12]; y = [2, 2]
-  // x = [0, 24], y = [2, 2]
+  glob = 2;
+  function();|\label{code:call2}|
 }
